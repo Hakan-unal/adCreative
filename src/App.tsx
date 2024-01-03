5fabd33cf215cde83c7fe9810c5cec82b3551fa1
Con Sınov <hakanunal_1996@hotmail.com>
2024-01-04 01:12:57 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { Row, Select,Checkbox, Col,Image } from "antd";
+import { Row, Select, Checkbox, Col, Image } from "antd";
 import "./index.css";
 import { getCharacter } from "rickmortyapi";
-import * as shlaami from "rickmortyapi"; // shlaami.getCharacter()
-import * as plumbus from "rickmortyapi"; // shlaami.getCharacter()
 import { useEffect, useState } from "react";
 
 export interface ResourceBase {
@@ -35,61 +33,87 @@ export interface ApiResponse<T> {
   statusMessage: string;
   /** The response that was provided by the API */
   data: T;
-
 }
 
 const App = () => {
-  const [data, setData] = useState<ApiResponse<Character[]>| any>([]);
-  const [selectedValues,setSelectedValues]=useState<any>([])
-  const handleChange = (value: string | string[]) => {
-    console.log(`Selected: ${value}`);
+  const [data, setData] = useState<ApiResponse<Character[]> | any>([]);
+  const [selectedValues, setSelectedValues] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    console.log( value)
-    setSelectedValues(value)
+
+  const handleChange = (value: string | string[]) => {
+    setSelectedValues(value);
   };
 
   const handleGetData = async () => {
-    const response = await getCharacter([2, 3, 4, 5]);
+    const response = await getCharacter([
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    ]);
 
-    const tempArr = response.data.map((obj,index) => (
-      {
-        label: obj.name,
-        value: obj.id,
-        test:obj.image
-      } 
-    ));
-
-    console.log(tempArr);
+    const tempArr = response.data.map((obj, index) => ({
+      value: JSON.stringify(obj),
+      label: obj.name,
+    }));
 
     setData(tempArr);
+    setIsLoading(false)
+
   };
 
+
   useEffect(() => {
-    handleGetData();
+    setIsLoading(true)
+
+    setTimeout(()=>{
+      handleGetData();
+
+    },1500)
+
   }, []);
 
   return (
     <Row justify={"center"}>
 
-
       <Select
+        loading={isLoading}
         mode="multiple"
         size="middle"
         onChange={handleChange}
-        style={{ width: 200 }}
+        style={{ width: 300 }}
         options={data}
-        
-        optionRender={(obj)=><Row>
-          <Col span={2}>
-          <Checkbox style={{cursor:"pointer"}} disabled checked={selectedValues.includes(obj.value)}/>
-          </Col>
-          <Col span={2} offset={1}>
-          </Col>
-          <Col span={2} offset={1}>
-          {obj.label}
-          </Col>
-
-          </Row>}
+        showSearch={true}
+        filterOption={(input, option) => {
+          return option?.label?.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+        }}
+        optionRender={(obj) => {
+          const tempData = obj.data.value;
+          const data = JSON.parse(obj.data.value);
+          const { image, name, episode } = data;
+          return (
+            <Row>
+              <Col span={2}>
+                <Checkbox
+                  style={{ cursor: "pointer" }}
+                  disabled
+                  checked={selectedValues.includes(tempData)}
+                />
+              </Col>
+              <Col span={4} offset={1}>
+                <Image
+                  preview={false}
+                  style={{ borderRadius: 5 }}
+                  src={image}
+                />
+              </Col>
+              <Col span={2} offset={1}>
+                {name}
+                <br />
+                <small>{episode.length} Episodes</small>
+              </Col>
+              <Col span={2} offset={1}></Col>
+            </Row>
+          );
+        }}
       />
     </Row>
   );
